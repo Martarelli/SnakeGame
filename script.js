cccabb828570a0e8046c8f8026761e5942c21350
Martarelli <renanmartarelli@gmail.com>
2021-05-18 18:54:34 -0300

    desenhar as comidas

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ snake[0] = {
 }
 
 let direction = "right";
-
-
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -23,6 +25,12 @@ function criarCobrinha (){
     }
 }
 
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box)
+
+}
+
 document.addEventListener('keydown', update);
 
 function update (event){
@@ -32,7 +40,6 @@ function update (event){
     if(event.keyCode == 40 && direction != "down") direction = "up";
 }
 
-
 function iniciarJogo(){
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
@@ -41,6 +48,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
